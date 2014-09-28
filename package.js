b97b83e727efe67a8c5f1dbdad6eb0ab457a4da8
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-09-28 20:05:14 +0530

    added rendering support

---
+++ package.js
@@ -0,0 +1,32 @@
+Package.describe({
+  "summary": "Server Side Rendering for Meteor",
+  "version": "1.0.0",
+  "git": "",
+  "name": "meteorhacks:ssr"
+});
+
+Package.onUse(function(api) {
+  configurePackage(api);
+  api.export(['Template']);
+});
+
+Package.onTest(function(api) {
+  configurePackage(api);
+  api.use([
+    'tinytest',
+  ], 'server');
+  
+  api.addFiles([
+    
+  ], 'server');
+});
+
+function configurePackage(api) {
+  api.versionsFrom('METEOR@0.9.2');
+  api.use(['blaze', 'spacebars'], 'server');
+  api.addFiles([
+    'lib/overrides.js',
+    'lib/template.js',
+    'lib/core.js'
+  ], 'server');
+}
